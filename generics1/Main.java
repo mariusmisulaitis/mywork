35cbcaca63bc47f72377714218057e4c06e4eb4b
TechBar <marius.misulaitis7@gmail.com>
2024-02-20 20:45:27 +0200

    kodo pataisymai (buvo ismesti varargs'ai)

--- generics1/Main.java
+++ generics1/Main.java
@@ -1,22 +1,24 @@
 package generics1;
 
-
 public class Main {
     public static void main(String[] args) {
 
         Integer[] skaiciai = {1, 2, 3};
         String[] zodziai = {"Hello", "World"};
 
-        spausdintiInfo(skaiciai, zodziai);
+        spausdintiBendraInfo(skaiciai, zodziai);
 
     }
 
-    public static <T> void spausdintiInfo(T[]... masyvai) {
-        for (T[] masyvas : masyvai) {
-            for (T elementas : masyvas) {
-                System.out.println(elementas + " ");
-            }
-            System.out.println();
+    public static <T> void spausdintiBendraInfo(T[] masyvas1, T[] masyvas2) {
+        spausdintiMasyvoInfo(masyvas1);
+        spausdintiMasyvoInfo(masyvas2);
+    }
+
+    private static <T> void spausdintiMasyvoInfo(T[] masyvas) {
+        for (T elementas : masyvas) {
+            System.out.println(elementas + " ");
         }
+        System.out.println();
     }
 }
